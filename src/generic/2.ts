7b9e485951710967f482da45545c459579c3ddab
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-28 17:08:47 +0300

    2 tasks si done

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,21 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
-  return {
+function compare<T extends AllType, U extends AllType>(
+  top: T,
+  bottom: U
+): AllType {
+  const result: Pick<AllType, keyof AllType> = {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
+
+  return result;
 }
 
 export {};
