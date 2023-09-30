9f1c0d6e013dcd828e341b6dc3b364dc3d2aeba9
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-30 11:48:41 +0300

    upd generic.1.ts

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,13 +3,13 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T extends [string, number]>(): Promise<T> {
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
-    resolve(["Text", 50] as T);
+    resolve(["Text", 50]);
   });
 }
 
-getPromise<[string, number]>().then((data) => {
+getPromise().then((data) => {
   console.log(data);
 });
 
